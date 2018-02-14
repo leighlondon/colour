23278caa0fc58c86192e81e8c1aa6c7110159228
Leigh London <leigh.00.london@gmail.com>
2018-02-14 12:08:05 +1100

    clock: set up a ticker

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,13 +1,26 @@
-// clock
+import * as React from "react";
 
-import * as React from 'react';
-
-class Clock extends React.Component<any, ClockState> {
-  constructor(props: any) {
+class Clock extends React.Component<Clock.Props, Clock.State> {
+  private timer: NodeJS.Timer;
+  constructor(props: Clock.Props) {
     super(props);
     this.state = {
       date: new Date()
     };
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
   }
 
   render() {
@@ -19,8 +32,11 @@ class Clock extends React.Component<any, ClockState> {
   }
 }
 
-interface ClockState {
-  date: Date;
+export namespace Clock {
+  export interface Props {}
+  export interface State {
+    date: Date;
+  }
 }
 
 export default Clock;
