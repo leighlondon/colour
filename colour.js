a3251f4d6d262bc65c8c4c230ec392ba2dc89329
Leigh London <leigh.00.london@gmail.com>
2018-02-14 21:04:17 +1100

    js: app update

--- colour.js
+++ colour.js
@@ -1,17 +1,21 @@
 (function() {
 
+  function zeropad(n) {
+    return n < 10 ? '0' + n : '' + n;
+  }
+
   function formatTimestamp(date) {
-    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    let h = zeropad(date.getHours());
+    let m = zeropad(date.getMinutes());
+    let s = zeropad(date.getSeconds());
+    return [h, m, s].join(':');
   }
 
   function tick() {
     let timestamp = formatTimestamp(new Date());
     let color = timestamp.split(':').join('');
-    let ts = document.getElementById("timestamp")
-    ts.innerText = timestamp;
-    ts.style.color = "white";
+    document.getElementById("timestamp").innerText = timestamp;
     document.body.style.backgroundColor = color;
-    console.log(color);
   }
 
   function main() {
