dd7da3bdb69661b328023a1b19f4e742d4f7e5d5
Leigh London <leigh.00.london@gmail.com>
2018-02-14 21:28:55 +1100

    js: update to app

--- colour.js
+++ colour.js
@@ -1,26 +1,37 @@
-(function() {
+/*
+ *       ___         ___                     ___         ___         ___
+ *      /\__\       /\  \                   /\  \       /\  \       /\  \
+ *     /:/  /      /::\  \                 /::\  \      \:\  \     /::\  \
+ *    /:/  /      /:/\:\  \               /:/\:\  \      \:\  \   /:/\:\__\
+ *   /:/  /  ___ /:/  \:\  \ ___     ___ /:/  \:\  \ ___  \:\  \ /:/ /:/  /
+ *  /:/__/  /\__/:/__/ \:\__/\  \   /\__/:/__/ \:\__/\  \  \:\__/:/_/:/__/___
+ *  \:\  \ /:/  \:\  \ /:/  \:\  \ /:/  \:\  \ /:/  \:\  \ /:/  \:\/:::::/  /
+ *   \:\  /:/  / \:\  /:/  / \:\  /:/  / \:\  /:/  / \:\  /:/  / \::/~~/~~~~
+ *    \:\/:/  /   \:\/:/  /   \:\/:/  /   \:\/:/  /   \:\/:/  /   \:\~~\
+ *     \::/  /     \::/  /     \::/  /     \::/  /     \::/  /     \:\__\
+ *      \/__/       \/__/       \/__/       \/__/       \/__/       \/__/
+ */
+(() => {
 
   function zeropad(n) {
     return n < 10 ? '0' + n : '' + n;
   }
 
-  function formatTimestamp(date) {
+  function to_hms(date) {
     let h = zeropad(date.getHours());
     let m = zeropad(date.getMinutes());
     let s = zeropad(date.getSeconds());
-    return [h, m, s].join(':');
+    return [h, m, s];
   }
 
   function tick() {
-    let timestamp = formatTimestamp(new Date());
-    let color = timestamp.split(':').join('');
-    document.getElementById("timestamp").innerText = timestamp;
-    document.body.style.backgroundColor = color;
+    let hms = to_hms(new Date());
+    document.getElementById("timestamp").innerText = hms.join(':');
+    document.body.style.backgroundColor = hms.join('');
   }
 
   function main() {
     setInterval(tick, 1000);
   }
-
   main();
 })();
