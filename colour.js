05f0c97faaf0711809f00e5b267039088ee8c9f2
Leigh London <leigh.00.london@gmail.com>
2018-04-22 14:25:41 +1000

    style: const

--- colour.js
+++ colour.js
@@ -12,26 +12,26 @@
  *      \/__/       \/__/       \/__/       \/__/       \/__/       \/__/
  */
 (() => {
-  function zeropad(n) {
+  const zeropad = n => {
     return n < 10 ? "0" + n : n;
-  }
+  };
 
-  function to_hms(date) {
+  const to_hms = date => {
     let h = zeropad(date.getHours());
     let m = zeropad(date.getMinutes());
     let s = zeropad(date.getSeconds());
     return [h, m, s];
-  }
+  };
 
-  function tick() {
+  const tick = () => {
     let hms = to_hms(new Date());
     document.getElementById("timestamp").innerText = hms.join(":");
     document.body.style.backgroundColor = "#" + hms.join("");
-  }
+  };
 
-  function main() {
+  const main = () => {
     tick();
     setInterval(tick, 1000);
-  }
+  };
   main();
 })();
